b61113dfdd6b636e7de157e11aed0afbb6bc84ed
Gerson Tiongson <gerson.tiongson.12314@gmail.com>
2025-09-28 12:04:27 +0800

    Re-add client folder as normal directory

---
+++ client/src/app/services/campgrounds.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Campground {
+  _id?: string;
+  name: string;
+  location: string;
+  description?: string;
+  price?: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CampgroundsService {
+  private apiUrl = 'http://localhost:3000/api/campgrounds'; // 🔹 replace with your backend URL
+
+  constructor(private http: HttpClient) {}
+
+  // Get all campgrounds
+  getCampgrounds(): Observable<Campground[]> {
+    return this.http.get<Campground[]>(this.apiUrl);
+  }
+
+  // Get single campground by ID
+  getCampground(id: string): Observable<Campground> {
+    return this.http.get<Campground>(`${this.apiUrl}/${id}`);
+  }
+
+  // Create a new campground
+  createCampground(campground: Campground): Observable<Campground> {
+    return this.http.post<Campground>(this.apiUrl, campground);
+  }
+
+  // Update an existing campground
+  updateCampground(id: string, campground: Campground): Observable<Campground> {
+    return this.http.put<Campground>(`${this.apiUrl}/${id}`, campground);
+  }
+
+  // Delete a campground
+  deleteCampground(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
