2741f5cb73422c3b7d13c741eaf797d6a92485d0
Gerson Tiongson <gerson.tiongson.12314@gmail.com>
2025-12-12 20:38:09 +0800

    remove redundant block

--- client/src/app/components/campground-details/campground-details.component.ts
+++ client/src/app/components/campground-details/campground-details.component.ts
@@ -5,12 +5,9 @@ import {
   OnInit,
   signal,
 } from '@angular/core';
-import { Campground, } from '../../models/campground.model';
+import { Campground } from '../../models/campground.model';
 import { ActivatedRoute, Router } from '@angular/router';
-import {
-  map,
-  Observable,
-} from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { MaterialElementsModule } from '../shared/material/material.module';
 import { MatDialog } from '@angular/material/dialog';
@@ -35,7 +32,7 @@ import { ReviewsComponent } from '../shared/components/reviews/reviews.component
     MaterialElementsModule,
     ɵInternalFormsSharedModule,
     ReactiveFormsModule,
-    ReviewsComponent
+    ReviewsComponent,
   ],
   templateUrl: './campground-details.component.html',
   styleUrls: ['./campground-details.component.scss'],
@@ -48,18 +45,13 @@ export class CampgroundDetailsComponent implements OnInit {
   private dialog = inject(MatDialog);
   private store = inject(Store);
   private storeService = inject(StoreService);
-  campId = toSignal(
-    this.activatedRoute.paramMap.pipe(map((params) => params.get('id')!)),
-    { initialValue: '' } // ✅ initialValue is valid here
-  );
+  campId = signal(this.activatedRoute.snapshot.paramMap.get('id')!);
   campground$!: Observable<Campground | undefined>;
   loading$ = this.storeService.loading$;
   imageLoading = true;
 
   ngOnInit() {
-    this.campId = signal(this.activatedRoute.snapshot.paramMap.get('id')!);
     this.store.dispatch(loadCampgroundById({ id: this.campId() }));
-
     // Subscribe to real-time changes from the store
     this.campground$ = this.store.select(selectCampgroundById(this.campId()));
   }
