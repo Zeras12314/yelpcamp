bce3abe1f88b15fb6dd7ce38aa2906ac35d57954
Gerson Tiongson <gerson.tiongson.12314@gmail.com>
2025-12-24 20:21:14 +0800

    styling part1

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/shared/components/header/header.component';
 import { FooterComponent } from './components/shared/components/footer/footer.component';
 import { Store } from '@ngrx/store';
 import { appInit } from './store/actions/user.action';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'yelpcamp';
   store = inject(Store);
-
+  router = inject(Router);
+  dispalyLandingImage(): boolean {
+    const url = this.router.url.toLowerCase();
+    return (
+      url.includes('login') ||
+      url.includes('register') ||
+      url === '/' ||
+      url.includes('home')
+    );
+  }
   ngOnInit(): void {
     this.store.dispatch(appInit());
   }
